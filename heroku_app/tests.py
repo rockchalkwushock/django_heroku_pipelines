110741666cb415a852fc01bceec2678d8308e1dc
Cody Brunner <rockchalkwushock@icloud.com>
2018-10-10 14:22:40 -0700

    adds simple test for heroku to process.

--- heroku_app/tests.py
+++ heroku_app/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def double(x):
+    return x * 2
+
+
+class MyTest(TestCase):
+    def test_double(self):
+        self.assertEqual(double(2), 4)
